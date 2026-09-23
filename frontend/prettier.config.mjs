0fd75082a564f4cfb7648db355b805e4125e4f3b
Ji JinChi <3109952@qq.com>
2026-09-23 14:59:35 +0800

    chore: 增加 Prettier 统一格式规则

---
+++ frontend/prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * 文件作用：统一前端代码格式，避免不同 IDE 和开发工具产生不同排版。
+ */
+export default {
+    semi: false,
+    singleQuote: false,
+    tabWidth: 4,
+    printWidth: 100,
+    trailingComma: "all",
+    bracketSpacing: true,
+    arrowParens: "always",
+    endOfLine: "lf",
+}
